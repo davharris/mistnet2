be839bb2b6d66964ce08c1831011999c0fb86228
David J. Harris <harry491@gmail.com>
2015-12-13 17:00:49 -0500

    add a basic logLik function

---
+++ R/logLik.R
@@ -0,0 +1,26 @@
+#' Calculate the log-likelihood of a network object
+#' @param object A \code{network} object
+#' @param state An optional \code{network_state} object. If not included,
+#'    state will be calculated by \code{\link{feedforward}}.
+#' @param include_penalties Should penalty terms be included in the
+#'    log-likelihood?
+#' @param ... Additional arguments (currently not used.)
+logLik.network = function(object, state, include_penalties = FALSE, ...){
+  if(missing(state)){
+    state = feedforward(network)
+  }
+
+  out = sum(
+    network$error_distribution$logLik(
+      # mu comes from the output of the last layer
+      mu = state$outputs[[length(state$outputs)]]
+    )
+  )
+
+  if(include_penalties){
+    stop("penalties haven't been implemented yet")
+    return(out + network$penalty)
+  }else{
+   return(out)
+  }
+}
