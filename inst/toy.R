6821762de257720c301161398ac3870ccf020ba1
David J. Harris <harry491@gmail.com>
2015-12-13 12:37:19 -0500

    'b' is ambiguous--biases or slopes. Use w instead

--- inst/toy.R
+++ inst/toy.R
@@ -2,7 +2,7 @@ devtools::load_all(".")
 library(gamlss.dist)
 library(optimx)
 
-n = 501
+n = 201
 n_x = 9
 n_z = 5
 n_y = 17
@@ -11,7 +11,7 @@ bd = 51
 # functions ---------------------------------------------------------------
 
 activator = sigmoid_activator
-
+1
 error_dist = BI()
 log_error_density = function(mu){
   dBI(x = y, mu = mu, bd = bd, log = TRUE)
@@ -21,30 +21,30 @@ density_grad = function(mu){
 }
 
 
-pre_activation = function(z, b){
-  cbind(x, z) %*% b
+pre_activation = function(z, w){
+  cbind(x, z) %*% w
 }
 
 loglik = function(p){
   z = relist(p, plist)$z
-  b = relist(p, plist)$b
+  w = relist(p, plist)$w
 
-  sum(log_error_density(activator$f(pre_activation(z, b))))
+  sum(log_error_density(activator$f(pre_activation(z, w))))
 }
 grad = function(p){
   z = relist(p, plist)$z
-  b = relist(p, plist)$b
-  act = pre_activation(z, b)
+  w = relist(p, plist)$w
+  act = pre_activation(z, w)
   mu = activator$f(act)
 
 
   coef_grad = activator$grad(act) * density_grad(mu)
-  input_grad = tcrossprod(coef_grad, b)
+  input_grad = tcrossprod(coef_grad, w)
 
 
   c(
     z = input_grad[ , -(1:ncol(x))],
-    b = crossprod(cbind(x, z), coef_grad)
+    w = crossprod(cbind(x, z), coef_grad)
   )
 }
 
@@ -53,22 +53,22 @@ grad = function(p){
 
 x = matrix(rnorm(n * n_x), ncol = n_x)
 true_z = matrix(rnorm(n * n_z, sd = .25), ncol = n_z)
-true_b = matrix(rnorm((n_x + n_z) * n_y, sd = .25), ncol = n_y)
+true_w = matrix(rnorm((n_x + n_z) * n_y, sd = .25), ncol = n_y)
 
 
 
 plist = list(
   z = true_z * 0 + rnorm(length(true_z), sd = .01),
-  b = true_b * 0 + rnorm(length(true_b), sd = .01)
+  w = true_w * 0 + rnorm(length(true_w), sd = .01)
 )
 start_p = unlist(plist)
-is_slope = grepl("^b", names(start_p))
-slope_is_for_observed = c(col(true_b) <= n_x)
+is_slope = grepl("^w", names(start_p))
+slope_is_for_observed = c(col(true_w) <= n_x)
 
 
 y = rBI(
   n = n_y * n,
-  mu = activator$f(pre_activation(true_z, true_b) + rnorm(n * n_y)),
+  mu = activator$f(pre_activation(true_z, true_w) + rnorm(n * n_y)),
   bd = bd
 )
 dim(y) = c(n, n_y)
@@ -90,6 +90,6 @@ o = optimx(
 estimates = relist(coef(o), plist)
 
 plot(
-  estimates$b[1:n_x, ],
-  c(true_b[1:n_x, ])
+  estimates$w[1:n_x, ],
+  c(true_w[1:n_x, ])
 )
