ba6eccb2123764b53b3b53732ab6458ad17c22d6
David J. Harris <harry491@gmail.com>
2015-12-14 20:21:41 -0500

    clarify loog density docs

--- R/log_density.R
+++ R/log_density.R
@@ -2,9 +2,9 @@
 #' @param network A \code{network} object
 #' @param state An optional \code{network_state} object. If not included,
 #'    state will be calculated by \code{\link{feedforward}}.
-#' @param include_penalties Should penalty terms be included in the
-#'    log-likelihood?
-#' @param par a vector of parameters
+#' @param include_penalties Should penalty terms (e.g. from prior distributions)
+#' be included in the log-density?
+#' @param par a vector of network parameters
 #' @param ... Additional arguments (currently not used.)
 log_density = function(network, state, par, include_penalties = FALSE, ...){
   if(missing(state)){
