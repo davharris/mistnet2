092a1089dd463663f0748915330e505b606711eb
David J. Harris <harry491@gmail.com>
2015-12-14 22:21:24 -0500

    clean up helpers.R, remove assert_that import

--- R/helpers.R
+++ R/helpers.R
@@ -1,12 +1,21 @@
+# Functions used internally that don't need to be made visible
+# to the user
+
+
+# Add each element of `vector` to the corresponding column, instead
+# of adding the whole vector to each column as the recycling rule would do
 `%plus%` = function(matrix, vector){
   rcpp_add_biases(matrix, vector)
 }
 
-#' @importFrom assertthat assert_that
+# several times faster than R's built-in `ifelse` function; useful for
+# activation functions that are called many times
 ifelse_matrix = function(test, yes, no){
 
   # Without this assertion, any nonzero entries would be coerced to TRUE
-  assert_that(is.logical(test))
+  if(!is.logical(test)){
+    stop("test is not logical")
+  }
 
   ifelse_matrix_cpp(test, yes, no)
 }
