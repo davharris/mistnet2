ef98d7ccdfdc96cb092a7465992e9d58dd679d75
David J. Harris <harry491@gmail.com>
2015-12-14 22:36:58 -0500

    add another test for ifelse_matrix

--- tests/testthat/test-ifelse_matrix.R
+++ tests/testthat/test-ifelse_matrix.R
@@ -14,4 +14,11 @@ test_that("ifelse_matrix gives the same answer as ifelse",{
     ifelse_matrix(x>1, sin(x), cos(x)),
     ifelse(x>1, sin(x), cos(x))
   )
+
+  # if `test` is numeric, all nonzero entries return TRUE.
+  # Test that ifelse_matrix rejects that case
+  expect_error(
+    ifelse_matrix(x, x, x),
+    "test is not logical"
+  )
 })
