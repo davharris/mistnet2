89ed461c54c2690cac16c0aa4386975ef7854bf9
David J. Harris <harry491@gmail.com>
2015-12-22 21:00:59 -0500

    more docs

--- R/error_distribution.R
+++ R/error_distribution.R
@@ -207,7 +207,12 @@ dldx_list = list(
 #'
 #' This distribution produces a flat prior with no bounds.  Its gradient and
 #' log-density are always zero. It's improper because its integral is not one
-#' (and is not finite).
+#' (and is not finite). Sampling from the distribution is not defined, so calling
+#' rIU results in an error.
+#' @param x vector of quantiles
+#' @param mu vector of location parameters (not used)
+#' @param n number of samples
+#' @param log logical; if TRUE, probabilities p are given as log(p)
 #' @export
 IU = function(){
   structure(
@@ -230,12 +235,16 @@ IU = function(){
 #' @export
 #' @rdname IU
 dIU = function(x, mu, log){
-  stopifnot(log)
-  rep(0, max(length(x), length(mu)))
+  if(log){
+    rep(0, max(length(x), length(mu)))
+  }else{
+    rep(1, max(length(x), length(mu)))
+  }
+
 }
 
 #' @export
 #' @rdname IU
-rIU = function(x, mu){
+rIU = function(n, mu){
   stop("Sampling is not defined for improper distributions")
 }
--- R/log_density.R
+++ R/log_density.R
@@ -14,7 +14,7 @@ log_density.error_distribution = function(object, adjusted_values, ...){
 
 
 #' Calculate the log-likelihood of a network object
-#' @param network A \code{network} object
+#' @param object A \code{network} object
 #' @param state An optional \code{network_state} object. If not included,
 #'    state will be calculated by \code{\link{feedforward}}.
 #' @param include_penalties Should penalty terms (e.g. from prior distributions)
