c8305c5c5ee5be1ac2fceffa603352a44d9bf0cf
David J. Harris <harry491@gmail.com>
2015-12-23 01:13:38 -0500

    improve print method

--- R/print.network.R
+++ R/print.network.R
@@ -1,20 +1,26 @@
 print.network = function(object){
+  cat(nrow(object$x), "observations\n\n")
+
   cat(
     ncol(object$x),
     "observed predictors and",
     ncol(object$par_skeleton$z),
-    "latent ones\n\n"
+    "latent variables\n\n"
   )
 
-  cat(
-    ncol(object$y),
-    "response variables with",
-    object$error_distribution$family[[2]],
-    "errors\n"
-  )
-
-  cat("\nlayers:\n")
+  cat("layers:\n")
   for(i in 1:length(object$activators)){
-    cat("  ", i, " ", object$activators[[i]]$name, "activation \n")
+    n_nodes = ncol(network$par_skeleton$weights[[i]])
+    cat(
+      "  ",
+      i,
+      ": ",
+      object$activators[[i]]$name,
+      " layer with ",
+      n_nodes,
+      " nodes\n",
+      sep = ""
+    )
   }
+  cat("    ", object$error_distribution$family[[2]], "error distribution")
 }
