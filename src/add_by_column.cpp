a496289482e95e9ecd61ccf638af5f1204b9c9c0
David J. Harris <harry491@gmail.com>
2015-12-24 15:17:06 -0500

    better error message in add_biases

--- src/add_by_column.cpp
+++ src/add_by_column.cpp
@@ -4,7 +4,7 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 NumericMatrix rcpp_add_biases(NumericMatrix m, NumericVector v) {
   if (m.ncol() != v.length()) {
-    stop("ncol(m) != length(v)");
+    stop("can't add biases when ncol(m) != length(v)");
   }
   NumericMatrix out(m.nrow(), m.ncol());
   for(int i = 0; i< m.ncol(); i++){
