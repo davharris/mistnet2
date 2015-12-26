41a05f080d146e659e919386614e9f33c3a55f65
David J. Harris <harry491@gmail.com>
2015-12-26 01:04:42 -0500

    fix namespace and docs

--- R/sample.R
+++ R/sample.R
@@ -1,10 +1,13 @@
+#' Draw random samples from an object
+#' @param object An object
+#' @param ... further arguments passed to or from other methods.
 #' @export
 draw_samples = function(object, ...){
   UseMethod("draw_samples")
 }
 
 #' Sample random numbers from a probability distribution
-#' @param distribution an \code{\link{distribution}} object
+#' @param object an \code{\link{distribution}} object
 #' @param ... additional arguments passed to \code{\link{get_values}}
 #' @export
 draw_samples.distribution = function(object, ...){
